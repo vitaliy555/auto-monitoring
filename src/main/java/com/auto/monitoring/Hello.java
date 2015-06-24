8b1bbe2a96860109ce2395bd9c737676833ea58c
Vitalii Chechel <Vitalii_Chechel@epam.com>
2015-06-24 17:41:09 +0300

    htmlunit

--- src/main/java/com/auto/monitoring/Hello.java
+++ src/main/java/com/auto/monitoring/Hello.java
@@ -1,5 +1,20 @@
 package com.auto.monitoring;
 
-public class Hello {
+import java.io.IOException;
 
-}
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jsoup.Jsoup;
+
+public class Hello {
+    public static void main(String[] args) throws IOException {
+         WebClient webClient = new WebClient();
+        webClient.setCssEnabled(false);
+        webClient.setJavaScriptEnabled(false);
+            final HtmlPage page = webClient.getPage("http://rst.ua/oldcars/mazda/");
+        System.out.println(page.getHtmlElementById("rst-ocid-4449245").getId());
+        page.getElementsByName("rst-ocb-i rst-ocb-i-premium rst-uix-radius");
+        }
+
+
+    }
